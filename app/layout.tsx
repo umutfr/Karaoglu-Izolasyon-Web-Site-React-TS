5c51bb2ad44a6ca7ef8934d20135fcaacaf555f3
DoomLaptop <umutw4141@gmail.com>
2025-10-18 22:07:02 +0300

    some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Karaoglu 3",
@@ -19,15 +12,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <div className="min-h-screen flex flex-col">
+          <Navbar/>
+            <main className="flex grow">{children}</main>
+          <Footer/>
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-      </div>
-    
+      Ana Sayfa
+    </div> 
   );
 }
